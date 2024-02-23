6bd95785ed0b83622062e43f055240b9302667bf
badrudeen20 <badruddinansary20@gmail.com>
2024-02-23 09:15:01 +0530

    vue-basic

---
+++ vue-basic/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HeaderBar from './components/HeaderBar.vue'
+
+</script>
+
+<template>
+    <div>
+      <HeaderBar />
+      <RouterView />
+    </div>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ vue-basic/src/components/HeaderBar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+defineProps({
+  pages: {
+    type: Array ,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                        <RouterLink to="/" class="nav-link">Home</RouterLink>
+                  </li>
+                  <li class="nav-item">
+                  <RouterLink to="/about" class="nav-link">About</RouterLink>
+                  </li>  
+            </ul>
+      </div>
+  </nav>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+</style>
